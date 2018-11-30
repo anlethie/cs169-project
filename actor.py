f96555204b94be699d39f254e12b66bb00738dd1
David Legg <davey.legg@gmail.com>
2018-11-29 17:03:21 -0800

    Genetic NN Actor written but not tested.

--- actor.py
+++ actor.py
@@ -42,6 +42,30 @@ class PerceptronActor(Actor):
                 i = j
         return i
 
+class NeuralNetActor(Actor):
+    """Actor that uses a neural network to react to observations."""
+    def __init__(self, observation_space, action_space, hidden_layers=[]):
+        """hidden_layers is a list of numbers, each number the number of nodes on a hidden layer, in order"""
+        super(NeuralNetActor, self).__init__(observation_space, action_space)
+        self._n_obs = product(observation_space.shape)
+        self._n_act = action_space.n
+        self._layers = []
+        for in_size,out_size in zip([self._n_obs] + hidden_layers, hidden_layers + [self._n_act]):
+            self._layers.append(np.random.random((out_size, in_size)))
+        self._threshold_fn = lambda X: 1. / (1. + np.exp(-X))
+
+    def react_to(self, observation):
+        current_vector = np.reshape(observation, self._n_obs)
+        for layer in self._layers:
+            current_vector = layer.dot(current_vector)
+            current_vector = self._threshold_fn(current_vector)
+        i = 0
+        for j,x in enumerate(current_vector):
+            if x > outputs[i]:
+                i = j
+        return i
+
+
 
 class GeneticPerceptronActor(PerceptronActor, GeneticActor):
     def get_genome(self):
@@ -51,3 +75,23 @@ class GeneticPerceptronActor(PerceptronActor, GeneticActor):
         pa = GeneticPerceptronActor(self._observation_space, self._action_space)
         pa._perceptron_matrix = (2 * np.reshape(genome.copy(), self._perceptron_matrix.shape)) - 1
         return pa
+
+class GeneticNNActor(NeuralNetActor, GeneticActor):
+    def get_genome(self):
+        genome = []
+        for layer in self._layers:
+            genome.append(np.reshape(layer.copy(), product(layer.shape)))
+        genome = (genome + 1.)/2.
+        return genome
+
+    def from_genome(self, genome):
+        genome = (genome.copy() * 2.) - 1.
+        nna = GeneticNNActor(self._observation_space, self._action_space)
+        nna._layers = []
+        start = 0
+        for layer in self._layers:
+            layer_size = product(layer.shape)
+            new_layer = np.reshape(genome[start:start+layer_size].copy(), layer.shape)
+            nna._layers.append(new_layer)
+            start += layer_size
+        return nna
